a88217d2a5d4c6976b04d835a064d6efedd0e5bb
MPLB-litvine <philipmakebeh@yahoo.com>
2018-03-12 08:56:34 +0100

    added new 237First wordpress folder

--- 237First_wordpress/wp-config.php
+++ 237First_wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '237first');
+define('DB_NAME', '237first_db');
 
 /** MySQL database username */
 define('DB_USER', 'iamadmin');
 
 /** MySQL database password */
-define('DB_PASSWORD', '1234qwerT');
+define('DB_PASSWORD', 'd8B*&pre55__pas5');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
